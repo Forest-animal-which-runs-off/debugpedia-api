0680b8d68bb0a4fcfa0972cfab8196b5369c9460
satojin219 <renzuoteng142@gmail.com>
2023-08-26 00:49:41 +0900

    feat: enable secure

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -52,7 +52,7 @@ func (uc *userController) Login(c echo.Context) error {
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
 	// NOTE:POSTmanを使う時はsecureをfalseにする
-	// cookie.Secure = true
+	cookie.Secure = true
 	cookie.HttpOnly = true
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
@@ -67,7 +67,7 @@ func (uc *userController) Logout(c echo.Context) error {
 	cookie.Expires = time.Now()
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
-	// cookie.Secure = true
+	cookie.Secure = true
 	cookie.HttpOnly = true
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
