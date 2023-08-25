35dc9fab8d063a0d76da17b257e17a1b0512de0b
satojin219 <renzuoteng142@gmail.com>
2023-08-25 23:58:37 +0900

    feat: add debugs router

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ import (
 func main() {
 	db := db.NewDB()
 	userRepository := repository.NewUserRepository(db)
+	debugRepository := repository.NewDebugRepository(db)
 	userUsecase := usecase.NewUserUsecase(userRepository)
+	debugUsecase := usecase.NewDebugUsecase(debugRepository)
 	userController := controller.NewUserController(userUsecase)
-	e := router.NewRouter(userController)
+	debugController := controller.NewDebugController(debugUsecase)
+	e := router.NewRouter(userController, debugController)
 	e.Logger.Fatal(e.Start(":8080"))
 }
--- router/router.go
+++ router/router.go
@@ -2,14 +2,30 @@ package router
 
 import (
 	"debugpedia-api/controller"
+	"os"
 
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 )
 
-func NewRouter(uc controller.IUserController) *echo.Echo {
+func NewRouter(uc controller.IUserController, dc controller.IDebugController) *echo.Echo {
 	e := echo.New()
 	e.POST("/signup", uc.SignUp)
 	e.POST("/login", uc.Login)
 	e.POST("/logout", uc.Logout)
+	d := e.Group("/debugs")
+	// echojwtのミドルウェアを使用。
+	d.Use(echojwt.WithConfig(echojwt.Config{
+		// jwtトークンを作る時と同じsecret
+		SigningKey: []byte(os.Getenv("SECRET")),
+		// クライアントから送られてくるjwtトークンがどこに格納されているかを指定
+		TokenLookup: "cookie:token",
+	}))
+	d.GET("", dc.GetAllDebugs)
+	d.GET("/:debugId", dc.GetDebugById)
+	d.POST("", dc.CreateDebug)
+	d.PUT("/:debugId", dc.UpdateDebug)
+	d.DELETE("/:debugId", dc.DeleteDebug)
+
 	return e
 }
