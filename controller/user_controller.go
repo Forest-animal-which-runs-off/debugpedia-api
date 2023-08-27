ab1c0677586eeca5bbc79c328869a7b7c45ec3fe
satojin219 <renzuoteng142@gmail.com>
2023-08-28 08:09:55 +0900

    fix: secure false

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -51,7 +51,7 @@ func (uc *userController) Login(c echo.Context) error {
 	cookie.Path = "/"
 	cookie.Domain = os.Getenv("API_DOMAIN")
 	// NOTE:POSTmanを使う時はsecureをfalseにする
-	cookie.Secure = true
+	// cookie.Secure = true
 	cookie.HttpOnly = true
 	cookie.SameSite = http.SameSiteNoneMode
 	c.SetCookie(cookie)
